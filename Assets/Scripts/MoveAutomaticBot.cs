c9698bd44d24b65ce8e641ed608570daa7f1f502
Domenico Esposito <mail@domenicoesposito.it>
2019-05-08 05:35:40 +0000

    No Comments

--- Assets/Scripts/MoveAutomaticBot.cs
+++ Assets/Scripts/MoveAutomaticBot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AutoMovePlayer : MonoBehaviour
+public class MoveAutomaticBot : MonoBehaviour
 {
 
     private GameObject[] pictures;
