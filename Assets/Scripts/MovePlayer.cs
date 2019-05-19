65ff202ac983ae36716cd271097d62cdc6d25383
Domenico Esposito <mail@domenicoesposito.it>
2019-05-19 06:52:59 +0000

    + Aggiunto script per voltarsi verso il quadro alla fine del percorso

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -17,7 +17,7 @@ public class MovePlayer : MonoBehaviour
     private readonly float angleForTurnLeft = 60f;
     private readonly float tolleranceDestination = 0.5f;
     private readonly float angleForTurnRight = 60f;
-    private readonly float angleForTurnBackforward = 150f;
+    private readonly float angleForTurnBack = 150f;
 
 
     private Vector3 localPos;
@@ -43,6 +43,14 @@ public class MovePlayer : MonoBehaviour
     }
 
 
+    private void OnCollisionStay(Collision collision)
+    {
+
+        TurnTowardsPicture(collision);
+
+    }
+
+
     private void Move()
     {
 
@@ -56,61 +64,49 @@ public class MovePlayer : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            RaycastHit hit;
-            Ray rayCameraToMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(rayCameraToMouse, out hit, 100, 1 << 10))
-            {
-                path = new NavMeshPath();
-                NavMesh.CalculatePath(transform.position, hit.point, 1, path);
-
-                firstCornerTarget = path.corners[1] - transform.position;
-                angleBetweenPlayerAndTarget = Vector3.Angle(transform.forward, firstCornerTarget);
-
-                localPos = transform.InverseTransformPoint(path.corners[1]);
-                Debug.Log("Lunghezza: " + path.corners.Length);
-                indexCornerPath = 1;
-            }
+            GeneratePathMousePosition();
         }
 
         DrawPath();
 
         if (CheckTurn())
         {
-            Debug.Log("Movimento - indexCornerPath: " + indexCornerPath + " | Corners: " + path.corners.Length);
-
-            playerRidiBody.MovePosition(Vector3.MoveTowards(transform.position, path.corners[indexCornerPath], Time.deltaTime * 5f));
-            animator.speed = 1f;
-
-            float distanceFromCorner = Vector3.Distance(transform.position, path.corners[indexCornerPath]);
-
-            if (distanceFromCorner > 0.1f)
-                RotationToTarget(path.corners[indexCornerPath], 10f);
-
-            // Incrementa punto di interesse
-            if (distanceFromCorner < 0.6f)
-            {
-                indexCornerPath++;
-
-                if (indexCornerPath > path.corners.Length - 1)
-                {
-                    Debug.Log("Movimento - Destinazione raggiunta");
-                    animator.speed = 0f;
-                    indexCornerPath = 1;
-                    path = null;
-                }
-            }
+            FollowPath();
         }
-
 
     }
 
 
-    private void RotationToTarget(Vector3 target, float speed)
+    private void TurnTowardsPicture(Collision collision)
     {
-        Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
-        targetRotation.x = 0f;
-        targetRotation.z = 0f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
+
+        if(path == null && collision.gameObject.CompareTag("Quadro"))
+        {
+
+            RectTransform picture = collision.gameObject.GetComponentInParent<RectTransform>();
+            RotationToTarget(picture.transform.position, 5f);
+            
+        }
+
+    }
+
+
+    private void GeneratePathMousePosition()
+    {
+        RaycastHit hit;
+        Ray rayCameraToMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(rayCameraToMouse, out hit, 100, 1 << 10))
+        {
+            path = new NavMeshPath();
+            NavMesh.CalculatePath(transform.position, hit.point, 1, path);
+            Debug.Log("Percorso - Lunghezza: " + path.corners.Length);
+
+            firstCornerTarget = path.corners[1] - transform.position;
+            angleBetweenPlayerAndTarget = Vector3.Angle(transform.forward, firstCornerTarget);
+
+            localPos = transform.InverseTransformPoint(path.corners[1]);
+            indexCornerPath = 1;
+        }
     }
 
 
@@ -124,52 +120,12 @@ public class MovePlayer : MonoBehaviour
     }
 
 
-    private void Turn()
+    private void RotationToTarget(Vector3 target, float speed)
     {
-        if (localPos == Vector3.zero)
-            return;
-
-        animator.speed = 0f;
-
-        if (localPos.x < tolleranceLeft)
-        {
-            TurnLeft();
-        }
-        else if (localPos.x > tolleranceRight)
-        {
-            TurnRight();
-        }
-        else
-        {
-            TurnBackforward();
-        }
-
-    }
-
-
-    private void TurnLeft()
-    {
-        if (angleBetweenPlayerAndTarget > angleForTurnLeft)
-            animator.animator.Play("TurnL", 0);
-    }
-
-
-    private void TurnRight()
-    {
-        if (angleBetweenPlayerAndTarget > angleForTurnRight)
-            animator.animator.Play("TurnR", 0);
-    }
-
-
-    private void TurnBackforward()
-    {
-        if (angleBetweenPlayerAndTarget > angleForTurnBackforward)
-        {
-            if (localPos.x < 0.0f)
-                animator.animator.Play("TurnTL", 0);
-            else
-                animator.animator.Play("TurnTR", 0);
-        }
+        Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
+        targetRotation.x = 0f;
+        targetRotation.z = 0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
     }
 
 
@@ -197,6 +153,83 @@ public class MovePlayer : MonoBehaviour
         localPos = Vector3.zero;
 
         return false;
+    }
+
+
+    private void FollowPath()
+    {
+        Debug.Log("Movimento - indexCornerPath: " + indexCornerPath + " | Corners: " + path.corners.Length);
+
+        playerRidiBody.MovePosition(Vector3.MoveTowards(transform.position, path.corners[indexCornerPath], Time.deltaTime * 5f));
+        animator.speed = 1f;
+
+        float distanceFromCorner = Vector3.Distance(transform.position, path.corners[indexCornerPath]);
+
+        if (distanceFromCorner > 0.1f)
+            RotationToTarget(path.corners[indexCornerPath], 10f);
+
+        // Incrementa punto di interesse
+        if (distanceFromCorner < 0.6f)
+        {
+            indexCornerPath++;
+
+            if (indexCornerPath > path.corners.Length - 1)
+            {
+                Debug.Log("Movimento - Destinazione raggiunta");
+                animator.speed = 0f;
+                indexCornerPath = 1;
+                path = null;
+            }
+        }
+    }
+
+
+    private void Turn()
+    {
+        if (localPos == Vector3.zero)
+            return;
+
+        animator.speed = 0f;
+
+        if (localPos.x < tolleranceLeft)
+        {
+            TurnLeft();
+        }
+        else if (localPos.x > tolleranceRight)
+        {
+            TurnRight();
+        }
+        else
+        {
+            TurnBack();
+        }
+
+    }
+
+
+    private void TurnLeft()
+    {
+        if (angleBetweenPlayerAndTarget > angleForTurnLeft)
+            animator.animator.Play("TurnL", 0);
+    }
+
+
+    private void TurnRight()
+    {
+        if (angleBetweenPlayerAndTarget > angleForTurnRight)
+            animator.animator.Play("TurnR", 0);
+    }
+
+
+    private void TurnBack()
+    {
+        if (angleBetweenPlayerAndTarget > angleForTurnBack)
+        {
+            if (localPos.x < 0.0f)
+                animator.animator.Play("TurnTL", 0);
+            else
+                animator.animator.Play("TurnTR", 0);
+        }
     }
 
 
