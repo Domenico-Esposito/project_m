fa19b7bb2e3ea2d3505a0853f91c9d44439e2611
Domenico Esposito <mail@domenicoesposito.it>
2019-08-11 18:09:38 +0000

    Refactoring

---
+++ Assets/Scripts/AntPattern_2.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AntPattern_2 : SeguiPercorso
+{
+    // Pattern movimento
+    private IEnumerator<GameObject> pictures;
+    private List<GameObject> walls = new List<GameObject>();
+    private Dictionary<GameObject, List<GameObject>> pictureOnWall = new Dictionary<GameObject, List<GameObject>>();
+    private GameObject currentWall;
+ 
+
+    public override void InitMovementPattern()
+    {
+        FindWallsWithPictures();
+        FindPicturesOnWalls();
+
+        currentWall = GetNextPicture().transform.parent.gameObject;
+        
+        pictureOnWall[currentWall].Sort(Distanza);
+        pictures = pictureOnWall[currentWall].GetEnumerator();
+
+    }
+
+    private void FindWallsWithPictures()
+    {
+        foreach (GameObject wall in GameObject.FindGameObjectsWithTag("Wall"))
+        {
+            if (wall.transform.childCount > 0)
+            {
+                walls.Add(wall);
+                pictureOnWall.Add(wall, new List<GameObject>());
+            }
+        }
+    }
+
+    private void FindPicturesOnWalls()
+    {
+        foreach (GameObject wall in walls)
+        {
+            foreach (Transform picture in wall.transform)
+            {
+                if (picture.gameObject.transform.GetChild(0).CompareTag("Quadro"))
+                    pictureOnWall[wall].Add(picture.gameObject);
+            }
+
+        }
+    }
+
+    private List<GameObject> GetWallsVisibleForBot()
+    {
+        int wallsLayer = 9;
+
+        Vector3[] directions = new Vector3[]{
+            -transform.right,
+            transform.right,
+            transform.forward,
+            -transform.forward
+/*             new Vector3(Mathf.Sin(22.5f * Mathf.Deg2Rad), 0, Mathf.Cos(22.5f * Mathf.Deg2Rad)),
+            - new Vector3(Mathf.Sin(22.5f * Mathf.Deg2Rad), 0, Mathf.Cos(22.5f * Mathf.Deg2Rad)),
+            new Vector3(Mathf.Sin(45 * Mathf.Deg2Rad), 0, Mathf.Cos(45 * Mathf.Deg2Rad)),
+            - new Vector3(Mathf.Sin(45 * Mathf.Deg2Rad), 0, Mathf.Cos(45 * Mathf.Deg2Rad)),
+            new Vector3(Mathf.Sin(67.5f * Mathf.Deg2Rad), 0, Mathf.Cos(67.5f * Mathf.Deg2Rad)),
+            -new Vector3(Mathf.Sin(67.5f * Mathf.Deg2Rad), 0, Mathf.Cos(67.5f * Mathf.Deg2Rad)),
+            new Vector3(Mathf.Sin(112.5f * Mathf.Deg2Rad), 0, Mathf.Cos(112.5f * Mathf.Deg2Rad)),
+            -new Vector3(Mathf.Sin(112.5f * Mathf.Deg2Rad), 0, Mathf.Cos(112.5f * Mathf.Deg2Rad)),
+            new Vector3(Mathf.Sin(135 * Mathf.Deg2Rad), 0, Mathf.Cos(135 * Mathf.Deg2Rad)),
+            - new Vector3(Mathf.Sin(135 * Mathf.Deg2Rad), 0, Mathf.Cos(135 * Mathf.Deg2Rad)),
+            new Vector3(Mathf.Sin(157.5f * Mathf.Deg2Rad), 0, Mathf.Cos(157.5f * Mathf.Deg2Rad)),
+            -new Vector3(Mathf.Sin(157.5f * Mathf.Deg2Rad), 0, Mathf.Cos(157.5f * Mathf.Deg2Rad)) */
+        };
+
+        List<GameObject> wallsCollision = new List<GameObject>();
+
+        foreach(Vector3 direction in directions)
+        {
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, 25f);
+            if (hits.Length > 0)
+            {
+                foreach (var hit in hits)
+                {
+                    if(hit.collider.gameObject.layer == wallsLayer){
+                        Debug.Log("Hit: " + hit.collider.name, hit.collider.gameObject);
+                        if(pictureOnWall.ContainsKey(hit.collider.gameObject)){
+                            if(pictureOnWall[hit.collider.gameObject].Count > 0){
+                                wallsCollision.Add(hit.collider.gameObject);
+                            }
+                        }
+                    }
+                }
+                    
+            }
+        }
+
+        return wallsCollision;
+    }
+
+    private GameObject GetNextPicture()
+    {
+        int nextPictureIndex = 0;
+        GameObject nextPicture = null;
+
+        List<GameObject> t = GetWallsVisibleForBot();
+
+        foreach(GameObject wall in t)
+        {
+            if (wall == null)
+                continue;
+
+            foreach(GameObject picture in pictureOnWall[wall])
+            {
+                if (picture.GetComponent<PictureInfo>().index < nextPictureIndex || nextPictureIndex == 0)
+                {
+                    nextPictureIndex = picture.GetComponent<PictureInfo>().index;
+                    nextPicture = picture;
+                }
+            }
+        }
+        
+        return nextPicture;
+    }
+
+    private GameObject GetClosestWall()
+    {
+        float minDistance = Mathf.Infinity;
+        GameObject closestWall = null;
+
+        foreach (GameObject wall in walls)
+        {
+            if (Vector3.Distance(wall.transform.position, transform.position) <= minDistance)
+            {
+                if(pictureOnWall[wall].Count > 0)
+                {
+                    closestWall = wall;
+                    minDistance = Vector3.Distance(wall.transform.position, transform.position);
+                }
+            }
+        }
+
+        return closestWall;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Uscita"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Quadro"))
+        {
+            // Il quadro non va visitato, non c'è nessun muro di interesse
+            if (currentWall == null)
+            {
+                return;
+            }
+
+            if (pictureOnWall[currentWall].Contains(pictures.Current))
+            {
+                pictureOnWall[currentWall].Remove(pictures.Current);
+
+                int currentPictureIndex = pictures.Current.GetComponent<PictureInfo>().index;
+
+                foreach(GameObject wall in pictureOnWall.Keys)
+                {
+                    pictureOnWall[wall].RemoveAll(
+                        delegate (GameObject x){
+
+                            if(x.gameObject.GetComponent<PictureInfo>().index < currentPictureIndex)
+                                return true;
+
+                            return false;
+                        });
+                }
+
+            }
+
+            pictures = pictureOnWall[currentWall].GetEnumerator();
+
+        }
+
+    }
+
+    public override GameObject GetNextDestination()
+    {
+        // Se ho visitato tutti i quadri sull'attuale muro
+        if (!pictures.MoveNext())
+        {
+            walls.Remove(currentWall);
+            currentWall = GetClosestWall();
+
+            // Se sono finiti i muri da visitare
+            if (currentWall == null || pictureOnWall[currentWall].Count == 0)
+            {
+                return GameObject.FindGameObjectWithTag("Uscita").gameObject;
+            }
+
+            pictureOnWall[currentWall].Sort(Distanza);
+
+            pictures = pictureOnWall[currentWall].GetEnumerator();
+            pictures.MoveNext();
+
+        }
+
+        return pictures.Current.transform.GetChild(0).gameObject;
+    }
+
+    private int Distanza(GameObject x, GameObject y)
+    {
+        float distance_1 = GetPathLength(x);
+        float distance_2 = GetPathLength(y);
+
+        if (distance_1 < distance_2) return -1;
+        if (distance_1 > distance_2) return 1;
+        return 0;
+    }
+
+    private int SortIndexPictures(GameObject x, GameObject y)
+    {
+        float distance_1 = x.GetComponent<PictureInfo>().index;
+        float distance_2 = y.GetComponent<PictureInfo>().index;
+
+        if (distance_1 < distance_2) return -1;
+        if (distance_1 > distance_2) return 1;
+        return 0;
+    }
+
+}
+
---
+++ Assets/Scripts/SeguiPercorso.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class SeguiPercorso : MonoBehaviour
+{
+    public float x = 0;
+    public float y = 0;
+    public float z = 0;
+
+    // Pattern movimento
+    private Rigidbody rigidBody;
+   
+    // Animazione
+    public AnimateCharacter generalAnimation;
+    public AnimationViewQuadro quadroViewAnimation;
+
+    // Segui percorso
+    public NavMeshPath path;
+    public Vector3 firstCornerTarget;
+    public int indexCornerPath = 1;
+    public float timedelta = 0f;
+
+    public abstract GameObject GetNextDestination();
+
+    public abstract void InitMovementPattern();
+
+    private void Awake()
+    {
+        InitAnimationBheavior();
+        InitMovementPattern();
+        GenerateNewPath();
+    }
+
+     public void InitAnimationBheavior(){
+        rigidBody = GetComponent<Rigidbody>();
+
+        generalAnimation = GetComponent<AnimateCharacter>();
+        generalAnimation.turnBack = true;
+        generalAnimation.tolleranceLeft = -3f;
+        generalAnimation.tolleranceRight = 3f;
+        generalAnimation.angleForTurnLeft = generalAnimation.angleForTurnRight = 60f;
+
+        quadroViewAnimation = GetComponent<AnimationViewQuadro>();
+        quadroViewAnimation.tolleranceLeft = -1.5f;
+        quadroViewAnimation.tolleranceRight = 1.5f;
+        quadroViewAnimation.angleForTurnLeft = quadroViewAnimation.angleForTurnRight = 50f;
+
+    }
+
+    private void Update()
+    {
+
+        Debug.DrawRay(transform.position,  new Vector3(x, y, z) * 3, Color.yellow);
+
+        Move();
+        generalAnimation.Animation();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        quadroViewAnimation.path = path;
+        quadroViewAnimation.TurnTowardsPicture(collision);
+        
+        if (collision.gameObject.CompareTag("Quadro"))
+        {
+            timedelta += Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Quadro") && path != null)
+        {
+            timedelta = 0f;
+        }
+    }
+
+    private void Move()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+
+        if (timedelta > 5f)
+        {
+            GenerateNewPath();
+            timedelta = 0f;            
+        }
+
+        DrawPath();
+        generalAnimation.Turn();
+
+        if (CheckTurn())
+        {
+            FollowPath();
+        }
+
+    }
+
+    public float GetPathLength(GameObject picture)
+    {
+        NavMeshPath p = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, picture.transform.GetChild(0).transform.position, 1, p);
+
+        float lng = 0;
+
+        for (int i = 0; i < p.corners.Length - 1; i++)
+        {
+            lng += Vector3.Distance(p.corners[i], p.corners[i + 1]);
+        }
+
+        Debug.Log("Lunghezza path: " + lng + " | Status: " + p.status, picture);
+
+        return lng;
+    }
+
+    private void GenerateNewPath()
+    {
+        path = new NavMeshPath();
+
+        NavMesh.CalculatePath(transform.position, RandomCoordinatesInFloorPicture(), 1, path);
+
+        firstCornerTarget = path.corners[1] - transform.position;
+        generalAnimation.angleBetweenPlayerAndTarget = Vector3.Angle(transform.forward, firstCornerTarget);
+        generalAnimation.localPos = transform.InverseTransformPoint(path.corners[1]);
+        indexCornerPath = 1;
+
+    }
+
+    public Vector3 RandomCoordinatesInFloorPicture()
+    {
+        GameObject next = GetNextDestination();
+
+        Collider floorPicture = next.GetComponent<Collider>();
+
+        Vector3 floorPictureSize = floorPicture.bounds.size;
+        float randomXInFloorPicture = Random.Range(-floorPictureSize.x / 2.5f, floorPictureSize.x / 2.5f);
+        float randomYInFloorPicture = Random.Range(-floorPictureSize.y / 2.5f, floorPictureSize.y / 2.5f);
+
+        Vector3 randomPositionInPlane = next.transform.position + new Vector3(randomXInFloorPicture, 0f, randomYInFloorPicture);
+
+        return randomPositionInPlane;
+
+    }
+
+
+    private void DrawPath()
+    {
+        if (path != null)
+        {
+            for (int i = 0; i < path.corners.Length - 1; i++)
+                Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+        }
+    }
+
+    private void RotationToTarget(Vector3 target, float speed)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
+        targetRotation.x = 0f;
+        targetRotation.z = 0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
+    }
+
+    private bool CheckTurn()
+    {
+        bool isRotation = generalAnimation.IsRotation();
+
+        if (!isRotation)
+            rigidBody.isKinematic = false;
+
+        if (!isRotation && path != null)
+        {
+            generalAnimation.angleBetweenPlayerAndTarget = Vector3.Angle(transform.forward, path.corners[indexCornerPath] - transform.position);
+            generalAnimation.localPos = transform.InverseTransformPoint(path.corners[indexCornerPath]);
+
+            if (generalAnimation.angleBetweenPlayerAndTarget > generalAnimation.angleForTurnLeft)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        generalAnimation.angleBetweenPlayerAndTarget = 0;
+        generalAnimation.localPos = Vector3.zero;
+
+        return false;
+    }
+
+    private void FollowPath()
+    {
+        rigidBody.MovePosition(Vector3.MoveTowards(transform.position, path.corners[indexCornerPath], Time.deltaTime * 5f));
+        generalAnimation.speed = 1f;
+
+        float distanceFromCorner = Vector3.Distance(transform.position, path.corners[indexCornerPath]);
+
+        if (distanceFromCorner > 0.1f)
+            RotationToTarget(path.corners[indexCornerPath], 10f);
+
+        // Passa al corner successivo
+        if (distanceFromCorner < 0.5f)
+        {
+            indexCornerPath++;
+
+            if (indexCornerPath > path.corners.Length - 1)
+            {
+                // Destinazione raggiunta
+                generalAnimation.speed = 0f;
+                indexCornerPath = 1;
+                path = null;
+            }
+        }
+    }
+
+}
